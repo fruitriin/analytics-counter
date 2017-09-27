2d10c53121edb6592f83117c0ee83b424c844666
Riin <nassii74@gmail.com>
2017-09-28 07:23:46 +0900

    fix count.php now responce json

--- count.php
+++ count.php
@@ -3,5 +3,5 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use AnalyticsCounter\Counter;
 
-$counter = new Counter();
-$counter->printResults($response);
+$counter = new Counter(__DIR__ . '/analyticsCredentials.json');
+echo json_encode($counter->printResults($response));
